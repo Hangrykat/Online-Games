79cc69a4ca73a1ef06089b6d54741fa6e877add5
Hangrykat <Hangrykat@users.noreply.github.com>
2020-10-21 12:54:19 +0200

    data taboo.js

---
+++ src/data/taboos.js
@@ -0,0 +1,124 @@
+const taboos = [
+    {
+        category: 'country',
+        id: 100,
+        word: 'Argentina',
+        tabooWords: ['wine', 'steak', 'Evita', 'America']
+    },
+    {
+        category: 'country',
+        id: 101,
+        word: 'Russia',
+        tabooWords: ['cold', 'Siberia', 'vodka', 'war']
+    },
+    {
+        category: 'country',
+        id: 103,
+        word: 'China',
+        tabooWords: ['cheap', 'made', 'corona', 'chopsticks']
+    },
+    {
+        category: 'country',
+        id: 104,
+        word: 'Bahamas',
+        tabooWords: ['beach', 'holiday', 'carribean', 'island']
+    },
+    {
+        category: 'country',
+        id: 105,
+        word: 'Saudi Arabia',
+        tabooWords: ['oil', 'camel', 'desert', 'war']
+    },
+    {
+        category: 'country',
+        id: 106,
+        word: 'Morocco',
+        tabooWords: ['Africa', 'tajine', 'market', 'desert']
+    },
+    {
+        category: 'country',
+        id: 107,
+        word: 'Thailand',
+        tabooWords: ['spicy', 'tuktuk', 'prostitute', 'beach']
+    },
+    {
+        category: 'country',
+        id: 108,
+        word: 'Japan',
+        tabooWords: ['sushi', 'anime', 'Fuji', 'Tokyo']
+    },
+    {
+        category: 'country',
+        id: 109,
+        word: 'Colombia',
+        tabooWords: ['drug', 'cartel', 'America', 'Che Guevara']
+    },
+    {
+        category: 'country',
+        id: 110,
+        word: 'Poland',
+        tabooWords: ['pierogi', 'vodka', 'borscht', 'pils']
+    },
+    {
+        category: 'country',
+        id: 111,
+        word: 'Greece',
+        tabooWords: ['gyros', 'beach', 'vacation', 'feta']
+    },       
+    {
+        category: 'country',
+        id: 112,
+        word: 'Indonesia',
+        tabooWords: ['sambal', 'Bali', 'surfing', 'Asia']
+    },
+    {
+        category: 'country',
+        id: 113,
+        word: 'New Zealand',
+        tabooWords: ['Hobbit', 'mountain', 'Kiwi', 'Australia']
+    }, 
+    {
+        category: 'country',
+        id: 114,
+        word: 'Canada',
+        tabooWords: ['hockey', 'Toronto', 'maple', 'Vancouver']
+    },
+    {
+        category: 'country',
+        id: 115,
+        word: 'Myanmar',
+        tabooWords: ['dictator', 'Asia', 'air ballon', 'temples']
+    },
+    {
+        category: 'country',
+        id: 116,
+        word: 'India',
+        tabooWords: ['curry', 'Bollywood', 'Asia', 'spicy']
+    },
+    {
+        category: 'country',
+        id: 117,
+        word: 'England',
+        tabooWords: ['queen', 'fish and chips', 'Big Ben', 'London']
+    },
+    {
+        category: 'country',
+        id: 118,
+        word: 'South Korea',
+        tabooWords: ['Kpop', 'ramen', 'BBQ', 'sushi']
+    },
+    {
+        category: 'country',
+        id: 119,
+        word: 'Mongolia',
+        tabooWords: ['desert', 'inner', 'camel', 'Asia']
+    },
+    {
+        category: 'country',
+        id: 120,
+        word: 'France',
+        tabooWords: ['Paris', 'cheese', 'baguette', 'Eiffel']
+    },
+]
+
+export default taboos
